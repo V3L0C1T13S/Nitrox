986703fb435917b9d2d916f7f3e4db2450124b15
V3L0C1T13S <turtletester20@gmail.com>
2022-04-07 09:01:50 -0400

    Add options to CLI

--- NitroxCLI/CLILogic.cs
+++ NitroxCLI/CLILogic.cs
@@ -1,4 +1,5 @@
 using System;
+using NitroxCLI;
 using NitroxCLI.Models.Patching;
 
 namespace NitroxCLI
@@ -6,6 +7,32 @@ namespace NitroxCLI
     internal class CLILogic : IDisposable
     {
         private NitroxEntryPatch nitroxEntryPatch;
+
+        private CLIConfig config;
+
+        public CLILogic()
+        {
+            
+        }
+
+        public void BeginPatch()
+        {
+            if (nitroxEntryPatch.IsApplied)
+            {
+                Console.WriteLine("Nitrox patch already applied.");
+                return;
+            }
+
+            Console.WriteLine("Applying Nitrox patch...");
+            nitroxEntryPatch.Apply();
+            Console.WriteLine("Nitrox patch applied.");
+        }
+
+        public void LaunchSubnautica() 
+        {
+            Console.WriteLine("Launching Subnautica...");
+            //Process.Start(config.SubnauticaPath);
+        }
 
         public void Dispose()
         {
--- NitroxCLI/Program.cs
+++ NitroxCLI/Program.cs
@@ -5,10 +5,42 @@ public static class Program
 { 
     public static void Main(string[] args) 
     {
-        CLILogic logic;
+        CLILogic logic = new CLILogic();
+
         if (Environment.OSVersion.Platform == PlatformID.Unix)
         {
             Console.WriteLine("Using Unix");
+        }
+
+        // Ask for what to do
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("1. Patch Subnautica");
+        Console.WriteLine("2. Unpatch Subnautica");
+        Console.WriteLine("3. Exit");
+
+        // Get input
+        string input = Console.ReadLine();
+
+        switch(input)
+        {
+            case "1":
+                logic.BeginPatch();
+                Console.WriteLine("Patched! Would you like to play now? (y/n)");
+                string play = Console.ReadLine();
+                if (play == "y")
+                {
+                    Console.WriteLine("Launching Subnautica...");
+                    logic.LaunchSubnautica();
+                }
+                break;
+            case "2":
+                logic.Dispose();
+                break;
+            case "3":
+                return;
+            default:
+                Console.WriteLine("Invalid input");
+                break;
         }
     }
 }
